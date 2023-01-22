3272fa8f2fb9ac0425b56674e3a82c4387813c26
IrregularBismuth <elliot.s@live.se>
2023-01-22 14:22:30 +0000

    auto commit

--- loginCheck.php
+++ loginCheck.php
@@ -1,25 +1,25 @@
 <?php
 session_start();
-//abc
 require_once "db/db.php";
-$sql="select * from users where binary name=:name and binary password=:password";
-$s=$dbc->prepare($sql);
+    
 $username=$_POST['name'];
-$password=$_POST['pass'];
-$s->bindValue(':name',$username);
-$s->bindValue(':password',$password);
-$s->execute();
-$result=$s->fetchAll();
-if($s->rowCount() > 0)
-{
-  echo $s->rowCount();
-
+ $password=$_POST['pass'];
+function login($name,$pass){
+    
+    $sql="select * from users where binary name=:name and binary password=:password";
+    $s=$dbc->prepare($sql);
+    $s->bindValue(':name',$name);
+    $s->bindValue(':password',$pass);
+    $s->execute();
+    $result=$s->fetchAll();
+    
+    if($s->rowCount() > 0)
+    {
+        echo $s->rowCount();
+        echo "sista test";
+     }
 }
-//foreach ($result as $result){
-//    echo $result['id'];
-//    echo "debug 44445";
-//    echo "</br>peeepo";
-//}
-//print_r($result);
+login($username,$password);
 ?>
+
 
