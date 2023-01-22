9148ab225cf04a381b739f75a29e44a31142d8ee
IrregularBismuth <elliot.s@live.se>
2023-01-22 13:27:33 +0000

    auto commit

--- loginCheck.php
+++ loginCheck.php
@@ -1,19 +1,34 @@
 <?php
 session_start();
-require_once("classes/accountHandler.php");
+//require_once("classes/accountHandler.php");
 $uname=$_POST['uname'];
 //echo $uname;
 $passw=$_POST['passw'];
+$sql="select * from users where binary name=:username and binary password=:password";
+$s->bindValue(":username",$uname);
+$s->bindValue(":password",$passw);
+$s->execute();
+$result=$s->fetchAll();
+if($s->rowCount($result) > 0 )
+{
+ echo "test";
+}
+
+
+
 //echo $uname;
 //echo $passw;
-loginFunc($uname,$passw);
-if(isset($_SESSION['username']))
 
-  header("Location: index.php");
-  exit(0);
-}
-header("Location: login.php?bad=1");
-exit(0);
+
+
+//loginFunc($uname,$passw);
+//if(isset($_SESSION['username']))
+
+ // header("Location: index.php");
+ // exit(0);
+//}
+//header("Location: login.php?bad=1");
+//exit(0);
 
 
 ?>
