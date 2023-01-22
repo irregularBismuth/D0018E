7854877dee32788dff774d85e09866e7f350cc67
IrregularBismuth <elliot.s@live.se>
2023-01-22 14:01:32 +0000

    auto commit

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ session_start();
         </title>
     </head>
     <body>
-        <form action="loginCheck.php" method="POST">
+        <form action="loginCheck.php" method="post">
             <label>Username </label><input type="text" name=username /> </br>
             <label>Password </label><input type="password" name=password /> </br>
             <input type="submit" name=submit /> </br>
