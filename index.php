e2320155a8e2516f6aa5aa691b218329857799fd
IrregularBismuth <elliot.s@live.se>
2023-01-22 13:46:41 +0000

    auto commit

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 session_start();
 //abc
 require_once "db/db.php";
-$sql="select id from users where binary name='C.lay' and password='lin alg';";
+$sql="select id from users where binary name=':t' and password=':u';";
 $s=$dbc->prepare($sql);
+$s->bindValue(":t","C.lay");
+$s->bindValue(":u","lin alg");
 $s->execute();
 $result=$s->fetchAll();
 foreach ($result as $result){
