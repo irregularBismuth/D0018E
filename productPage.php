0fe155743fd3daaa1baeb212d398503d5f132bc2
wiiggee1 <viktor@smedberg.eu>
2023-01-23 10:49:21 +0000

    auto commit

--- productPage.php
+++ productPage.php
@@ -27,10 +27,12 @@ session_start();
             $query_output = $fetch_result->fetchAll();
                 foreach ($query_output as $query_output) {
                 # code...
-               echo $query_output["animal_name"]."<br>".$query_output["animal_price"]."<br>".$query_output["animal_category"];
-            }
-            
+               echo $query_output["animal_name"]."<img src=".$query_output['animal_image']."/>"."<br>".$query_output["animal_price"]."<br>".$query_output["animal_category"];
+               $images = $query_output["animal_image"];
+
+            } 
         ?>
+
     </body>
 
 </html>
