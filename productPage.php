2f89d2b5a342abced0278a1506b9924ea3d918a0
wiiggee1 <viktor@smedberg.eu>
2023-01-26 13:33:04 +0000

    auto commit

--- productPage.php
+++ productPage.php
@@ -6,7 +6,7 @@ session_start();
 
     <head>
        
-     <title> 123445555555555  pepo ProductPage </title>
+     <title> 123445555  pepo ProductPage </title>
     <link rel="stylesheet" type="text/css" target="cartPage" href="shoppingCartStyle.css">
     </head>
 
