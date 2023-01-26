a9b95c8fef72c28a3183254259cd053d2a2ae53c
wiiggee1 <viktor@smedberg.eu>
2023-01-26 13:32:43 +0000

    auto commit

--- productPage.php
+++ productPage.php
@@ -15,56 +15,24 @@ session_start();
         <h1> Product Page (shopping cart) </h1>
         
         <?php
-
-            echo '
-                <div class="row">
-
-                    <div class="columnCart">
-                       <div class="boxFrame">
-                            <div class: "columnImage">
-                                IMAGE HERE
-                            </div>
-                            <div class: "columnData">
-                                DATA HERE
-                            </div>
-    
-                            <button> add </button> <br> 
-                            <button> checkout </button> <br> 
-                            <button> remove </button> <br>
-                            <button> info </button> 
-                        </div>  
-                    </div>
-
-                    <div class="columnCart">
-                       <div class="boxFrame">
-                            <button> add </button> <br> 
-                            <button> checkout </button> <br> 
-                            <button> remove </button> <br>
-                            <button> info </button> 
-                        </div> 
-                    </div>
-
-                    <div class="columnCart">
-                        <div class="boxFrame">
-                            <button> add </button> <br> 
-                            <button> checkout </button> <br> 
-                            <button> remove </button> <br>
-                            <button> info </button> 
+            
+            function generateBoxFrames($countRow){
+                for ($i = 0; $i<= $countRow;$i++){
+                    echo '
+                    <div class="row">
+                        <div class="columnCart">
+                            <div class="boxFrame">
+                                <button> add </button> <br> 
+                                <button> checkout </button> <br> 
+                                <button> remove </button> <br>
+                                <button> info </button> 
+                            </div> 
                         </div>
-                    </div> 
-
-                    <div class="columnCart">
-                       <div class="boxFrame">
-                            <button> add </button> <br> 
-                            <button> checkout </button> <br> 
-                            <button> remove </button> <br>
-                            <button> info </button> 
-                        </div> 
                     </div>
-        
-                </div>';
-
-            echo "<br>";  
+                    <br>';
+                }
+            };
+               
 
             // Will add better logic to this:
             require_once("db/db.php");
@@ -76,7 +44,7 @@ session_start();
             $queryItemId = "SELECT * FROM animals WHERE item_id = animal_id";
             $fetch_result = $connection_obj->query($querySql);
             $query_output = $fetch_result->fetchAll();
-
+            $count=$fetch_result->rowCount();
                 foreach ($query_output as $query_output) {
                 # code...
                echo "Animal type: ".$query_output["animal_name"]."<br>".'<img src="'.$query_output['animal_image'].'" alt="image" width="'.$width.'" height="'.$height.'">'."<br>"."Price: ".$query_output["animal_price"]."<br>"."Description: ".$query_output["animal_category"]."<br>";
