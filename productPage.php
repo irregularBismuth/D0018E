db6d43af0bc35fbc76b736e441261e7afed18ad0
wiiggee1 <viktor@smedberg.eu>
2023-01-26 15:21:50 +0000

    auto commit

--- productPage.php
+++ productPage.php
@@ -62,7 +62,7 @@ session_start();
                 echo "Animal type: ".$query_output["animal_name"]."<br>".'<img src="'.$query_output['animal_image'].'" alt="image" width="'.$width.'" height="'.$height.'">'."<br>"."Price: ".$query_output["animal_price"]."<br>"."Description: ".$query_output["animal_category"]."<br>";
 
 
-                array_push($images_array,'<img src="'.$query_output['animal_image'].'" alt=image" width="'.$width.'" height"'.$height.'">');
+                array_push($images_array,('<img src="'.$query_output['animal_image'].'" alt=image" width="'.$width.'" height"'.$height.'">'));
 
                 array_push($info_array,'Animal type: '.$query_output["animal_name"]."<br>"."Price: ".$query_output["animal_price"]."<br"."Description: ".$query_output["animal_category"]);
                 generateBoxFrames($count, $images_array, $info_array);
