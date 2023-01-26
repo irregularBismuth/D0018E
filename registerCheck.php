1b99296cfdd6e1f4a14fce97445f2c77d7f1ac75
IrregularBismuth <elliot.s@live.se>
2023-01-26 13:47:51 +0000

    auto commit

--- registerCheck.php
+++ registerCheck.php
@@ -1,8 +1,13 @@
 <?php
 session_start();
 $name=$_POST['name'];
-$pass=$_POST['pasw'];
-
+$pass=$_POST['pass'];
+$spass=$_POST['samePass'];
+if($pass!=$spass)
+{
+    header("Location: register.php?bad=1");
+    exit(0);
+}
 
 
 
