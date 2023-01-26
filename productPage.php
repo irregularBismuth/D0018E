42b678fb55e840fcae6820d1612d6416a6d6cd83
wiiggee1 <viktor@smedberg.eu>
2023-01-26 16:45:11 +0000

    auto commit

--- productPage.php
+++ productPage.php
@@ -60,13 +60,12 @@ session_start();
 
                 foreach ($query_output as $query_output) {
                 # code...
-                echo "Animal type: ".$query_output["animal_name"]."<br>".'<img src="'.$query_output['animal_image'].'" alt="image" width="'.$width.'" height="'.$height.'">'."<br>"."Price: ".$query_output["animal_price"]."<br>"."Description: ".$query_output["animal_category"]."<br>";
 
+                    array_push($images_array, '<img src="'.$query_output['animal_image'].'" alt=image" width="'.$width.'" height"'.$height.'">');
 
-                array_push($images_array, '<img src="'.$query_output['animal_image'].'" alt=image" width="'.$width.'" height"'.$height.'">');
-
-                array_push($info_array,'Animal type: '.$query_output["animal_name"]."<br>"."Price: ".$query_output["animal_price"]."<br>"."Description: ".$query_output["animal_category"]);
-                generateBoxFrames($count, $images_array, $info_array);
+                    array_push($info_array,'Animal type: '.$query_output["animal_name"]."<br>"."Price: ".$query_output["animal_price"]."<br>"."Description: ".$query_output["animal_category"]);
+                
+                    generateBoxFrames($count, $images_array, $info_array);
 
 
             }
