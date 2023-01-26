d10944ce993558efd835c150132a4eb83970a224
wiiggee1 <viktor@smedberg.eu>
2023-01-26 17:41:22 +0000

    auto commit

--- productPage.php
+++ productPage.php
@@ -24,7 +24,7 @@ session_start();
                             <div class="boxFrame">
                                 <div class="">'.$image[$i].'</div> 
                                 <br>
-                                <div class="infoSection">'.$info[$i].'</div>            
+                                <div class="infoStyle">'.$info[$i].'</div>            
                                 <br>
                                 <button> add </button>  
                                 <button> checkout </button> 
