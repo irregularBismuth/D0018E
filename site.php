2bfb048d414729ac55d04510609c8ac5200ab799
IrregularBismuth <elliot.s@live.se>
2023-01-26 13:20:57 +0000

    auto commit

--- site.php
+++ site.php
@@ -1,37 +1,15 @@
 <!DOCTYPE html>
 <html>
+<head>
+<link rel="stylesheet" type="text/css" class="" href="style.css" media="screen" />
+</head>
 <style>
 
 
 
 </style>
-<!--
-<header class="headern">i
-reareareaerare
-<link rel="stylesheet" type="text/css" class="" href="style.css" media="screen" />
- 
-    <a href="site.php"><img src="logo.png" width="400"></a>
- 
-    <form role="search" id="form">
-  <input type="search" id="query" name="q"
-   placeholder="Search..."
-   aria-label="Search through site content">
-  <button>
-    <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
-  </button>
-</form>
-
-
-    <a href="login.php"> Login </a>
-
-
-</header>
--->
 <body>
-
-
 <header class="headern">
-<link rel="stylesheet" type="text/css" class="" href="style.css" media="screen" />
  
     <a href="site.php"><img src="logo.png" width="400"></a>
  
