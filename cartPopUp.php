3b595a98be0dac85d7cedafe8bb1a8ab78d7232e
wiiggee1 <viktor@smedberg.eu>
2023-01-27 17:50:54 +0000

    auto commit

--- cartPopUp.php
+++ cartPopUp.php
@@ -22,8 +22,8 @@ session_start();
             //}
            echo '
                 <div class="productCart">
-                    <button type="button" onclick='.doSomething().'>
-                        <img type="img" src="shoppingcart.jpg" alt ="Image" width=80, height=80>
+                    <button class="button" onclick='.doSomething().'>
+                        <img src="shoppingcart.jpg" alt ="Image" width=80, height=80>
                     </button>
                 </div>
                 '  
