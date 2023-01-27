5bdfd14c83ac130e6b03f6fdf725c8a9e44c913c
wiiggee1 <viktor@smedberg.eu>
2023-01-27 17:03:59 +0000

    auto commit

--- cartPopUp.php
+++ cartPopUp.php
@@ -13,8 +13,17 @@ session_start();
     <body>
 
         <?php
-            
-    
+            function doSomething(){
+                    
+                }
+            //div productCart should be the layout section
+            //button tag should also have a class
+            //}
+           echo '
+                <div id="productCart">
+                    <button onclick'.doSomething().'</button>
+                </div>
+                '  
         
         ?>
 
