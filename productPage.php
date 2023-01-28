e24dfb6da24c76137147d5f03f37ae1d12764c9b
wiiggee1 <viktor@smedberg.eu>
2023-01-28 18:20:36 +0000

    auto commit

--- productPage.php
+++ productPage.php
@@ -84,6 +84,7 @@ session_start();
                
                 }
                 require_once("db/db.php");
+                require_once("SQLHandler.php");
                 $sql_obj = new SQLHandler($dbc);
                 $animals = $sql_obj->get_product_data();
                 $row_count = count($animals);
