73bdaeee89a2ce9657c262cc126e9ac3930f4d2d
wiiggee1 <viktor@smedberg.eu>
2023-01-28 18:30:36 +0000

    auto commit

--- productPage.php
+++ productPage.php
@@ -84,10 +84,10 @@ session_start();
                
                 }
                 require_once("db/db.php");
-                require_once("SQLHandler.php");
-                $sql_obj = new SQLHandler($dbc);
-                $animals = $sql_obj->get_product_data();
-                $row_count = count($animals['image']);
+                //require_once("SQLHandler.php");
+                //$sql_obj = new SQLHandler($dbc);
+                //$animals = $sql_obj->get_product_data();
+                //$row_count = count($animals['image']);
                 //generateBoxFrames($row_count, $animals);         
                 generateBoxFrames($count, $animal_array);
         ?>
