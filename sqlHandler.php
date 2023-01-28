bfdfe0a5c8f62004eb2496fe6daa4301886f249d
wiiggee1 <viktor@smedberg.eu>
2023-01-28 17:53:47 +0000

    auto commit

--- sqlHandler.php
+++ sqlHandler.php
@@ -17,20 +17,18 @@ session_start();
     class SQLHandler {
         public $db_connector;
         public $query_array;
-        public $animal_array;
 
         function __construct($db_connector)
         {
             $this->db_connector = $db_connector;
             $this->query_array = []; //initilize an empty array that should store unique users queries.
-            $this->animal_array = array("name"=>[],"price"=>[],"image"=>[], "category"=>[]);
         }
     
         function get_product_data(){
             /*
             This metod should request query from our database that show product/item data on our site
             */
-            
+            $animal_array = array("name"=>[],"price"=>[],"image"=>[], "category"=>[]);
             $query = "SELECT * FROM animals";
             $queryUser = "SELECT * FROM animals WHERE item_id = animal_id";
         
@@ -39,12 +37,12 @@ session_start();
             $output = $fetch_result->fetchAll();
 
             foreach($output as $output){
-                array_push($this->animal_array['image'],$output['animal_image']);
-                array_push($this->animal_array['name'],$output['animal_name']);
-                array_push($this->animal_array['price'],$output['animal_price']);
-                array_push($this->animal_array['category'],$output['animal_category']);
+                array_push($animal_array['image'],$output['animal_image']);
+                array_push($animal_array['name'],$output['animal_name']);
+                array_push($animal_array['price'],$output['animal_price']);
+                array_push($animal_array['category'],$output['animal_category']);
             }
-            return $this->animal_array;
+            return $animal_array;
 
         }
     }
