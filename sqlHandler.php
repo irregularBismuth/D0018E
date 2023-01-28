2f0968063a3a46faaa6b542fb7bb7cb0767a7377
wiiggee1 <viktor@smedberg.eu>
2023-01-28 16:18:27 +0000

    auto commit

---
+++ sqlHandler.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<?php
+session_start();
+?>
+
+
+<?php
+
+class SQLHandler {
+    public $test = require_once("db/db.php").$dbc;
+    public $sql;
+    public $query;
+
+    function __construct()
+    {
+        $this->sql = $this->test; 
+
+    }
+    
+    public function get_query(){
+        $query = "SELECT * FROM animals";
+        
+        $fetch_result = $this->sql->query($query);
+        $row_count = $fetch_result->rowCount();
+        $output = $fetch_result->fetchAll();
+
+        foreach($output as $output){
+            echo $output['animal_name'];
+        }
+
+    }
+}
+
+$obj = new SQLHandler();
+echo $obj->get_query(); 
+
+
+?>
