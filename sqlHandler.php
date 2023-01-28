8faaf0fcca63e873fcbacc20bfb930648a78dc87
wiiggee1 <viktor@smedberg.eu>
2023-01-28 18:03:32 +0000

    auto commit

--- sqlHandler.php
+++ sqlHandler.php
@@ -48,7 +48,8 @@ session_start();
         }
     }
     $obj = new SQLHandler($dbc);
-    echo $obj->get_product_data()["price"];
+    $animals = $obj->get_product_data();
+    echo $animals['name'];
     
     ?>
     
