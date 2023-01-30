e41a0082721ceab8cbd04c39793144642b71cf52
IrregularBismuth <elliot.s@live.se>
2023-01-30 08:28:39 +0000

    auto commit

--- productPage.php
+++ productPage.php
@@ -85,11 +85,11 @@ session_start();
                 }
                 require_once("db/db.php");
                 //require_once("SQLHandler.php");
-                $sql_obj = new SQLHandler($dbc);
-                $animals = $sql_obj->get_product_data();
-                $row_count = count($animals['image']);
-                generateBoxFrames($row_count, $animals);         
-                //generateBoxFrames($count, $animal_array);
+               // $sql_obj = new SQLHandler($dbc);
+              //  $animals = $sql_obj->get_product_data();
+               // $row_count = count($animals['image']);
+               // generateBoxFrames($row_count, $animals);         
+                generateBoxFrames($count, $animal_array);
         ?>
 
     </body>
