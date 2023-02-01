b4dd83c10f05136deceae057d0022ff0868ae698
wiiggee1 <viktor@smedberg.eu>
2023-02-01 15:33:29 +0000

    auto commit

--- cartPopUp.php
+++ cartPopUp.php
@@ -6,23 +6,20 @@ session_start();
 <html>
 
     <head>
-        <title>....</title>
+        <title></title>
         <link rel="stylesheet" type="text/css" target="cartPopup" href="popup.css">
     </head>
     
     
     <body>
 
-        <?php
-            function changeToProductPage(){
-                    echo '<a href=productPage.php>';
-                }
+        <?php                 
             function generateCartButton(){
                 echo '
                     <div class="productCart">
-                        <button class="button" onclick='.changeToProductPage().'>
-                        <img src="shoppingcart.jpg" alt ="Image" width=80, height=80>
-                        </button>
+                        <a href="productPage.php">
+                        <img src="shoppingcart.jpg" alt ="Image" width=70, height=70>
+                        </a>
                     </div>'; 
             }
         ?>
