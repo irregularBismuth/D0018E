4b1b3dcb5613afb1a371705d1304a1e64839bc88
wiiggee1 <viktor@smedberg.eu>
2023-02-01 15:27:45 +0000

    auto commit

--- productPage.php
+++ productPage.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <?php
 session_start();
+require_once("cartPopUp.php");
 require_once("sqlHandler.php");
 $animals = $sqlHandler->get_product_data();
 $row_count = count($animals['image']);
@@ -24,6 +25,7 @@ $row_count = count($animals['image']);
                 <img src="logo.png" width="400">
             </a>
             <h3>Product cart</h3>
+            <?php generateCartButton() ?>
             <form role="search" id="form">
                 <input type="search" id="query" name="q" placeholder="Search...">
             </form>
