4a0a8df86878ef423cae3c064afbe6cd2de0eb43
wiiggee1 <viktor@smedberg.eu>
2023-02-01 14:57:42 +0000

    auto commit

---
+++ userNavigationMenu.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<?php
+session_start();
+require_once("sqlHandler.php");
+require_once("userProfile.php");
+?>
+<html>
+
+    <head>
+    <link
+     <title> User navigation menu </title>
+    
+    </head>
+
+    <body>
+        <div class="headern">
+            <a href="site.php">
+                <img src="logo.png" width="400">
+</a>
+            </a>
+        </div>
+
+    
+        <?php
+            //code here
+        ?>
+
+    </body>
+</html>
+
+
