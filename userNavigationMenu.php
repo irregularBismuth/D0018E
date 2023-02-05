58ef8cdb86da5551283facb7480ba89797278ac8
wiiggee1 <viktor@smedberg.eu>
2023-02-05 19:11:07 +0000

    auto

--- userNavigationMenu.php
+++ userNavigationMenu.php
@@ -22,7 +22,7 @@ require_once("userProfile.php");
 
         <!-- <img src="PHPQUERY TO GET PROFILE IMG"> -->
         <br>
-        Hello: <?php $userProfile->getSessionData("name");?>
+        Hello: <?php echo $userProfile->getSessionData("name");?>
         <br>
         <img src="balanceIcon.png" width="50" height="50"> 
         Balance: MAKE QUERY HERE!     
