ba95b4e2ae61a3fbbc78a514c78377317de5bdbf
wiiggee1 <viktor@smedberg.eu>
2023-02-09 13:39:45 +0000

    auto

--- checkSession.php
+++ checkSession.php
@@ -2,7 +2,7 @@
 session_start();
 function checkSession(){
  
-    if(!(isset($_SESSION['username']))){
+    if(!(isset($_SESSION['id']))){
         header("Location: login.php");
         exit(0);
     }
