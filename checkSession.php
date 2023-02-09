cb96d9db21210bb52f42c775e3bd06a266b8ce4d
wiiggee1 <viktor@smedberg.eu>
2023-02-09 13:47:19 +0000

    auto

--- checkSession.php
+++ checkSession.php
@@ -8,7 +8,7 @@ function checkSession(){
     }
     else{
         $site_url = "http://130.240.200.85/d0018e/D0018E/site.php";
-        header("Location: ".$site_url, true, 301);
+        header("Location: productPage.php");
         exit(0);
     }
     
