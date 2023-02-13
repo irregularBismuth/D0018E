d687586aca4e79f88e05a53df78213c379c780b7
wiiggee1 <viktor@smedberg.eu>
2023-02-13 15:47:44 +0000

    auto commit

--- site.php
+++ site.php
@@ -20,7 +20,7 @@ require_once("cartMenuSwitch.php");
  
     <a href="site.php"><img src="../images/logo.png" width="400"></a>
     <?php generateCartButton()?>
-    <?php echo $_SESSION['username']?>
+    <?php echo "Welcome: ".$_SESSION['username']?>
     <form role="search" id="form">
   <input type="search" id="query" name="q"
    placeholder="Search..."
