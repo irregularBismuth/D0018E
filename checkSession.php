2a9e905cf48c682fccf7033ab39e7b9b856340a0
IrregularBismuth <elliot.s@live.se>
2023-02-13 15:27:48 +0000

    auto commit

--- checkSession.php
+++ checkSession.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-<<<<<<< HEAD
 function checkSession()
 {
  if(!(isset($_SESSION['username'])))
@@ -12,22 +11,6 @@ function checkSession()
     header("Location: site.php");
     exit(0);
 }
-
-=======
-function checkSession(){
- 
-    if(!(isset($_SESSION['id']))){
-        header("Location: login.php");
-        exit(0);
-    }
-    else{
-        $site_url = "http://130.240.200.85/d0018e/D0018E/site.php";
-        header("Location: productPage.php");
-        exit(0);
-        //adahsduggrsh
-    }
-    
->>>>>>> 03fa6cd1c50c647db3b8fad79d9e8657c0a72323
-}
+} 
 //peepo test
 ?>
