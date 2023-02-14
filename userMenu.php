a9cf16d3c269d809644bee783090b690b5b352d7
wiiggee1 <viktor@smedberg.eu>
2023-02-14 10:34:13 +0000

    auto commit

---
+++ userMenu.php
@@ -0,0 +1,21 @@
+<?php 
+session_start();
+require_once("userProfile.php");
+?>
+
+<html>
+    
+    <head>
+        <title> UserMenu </title>
+        <link rel="stylesheet" type="text/css" href="style/userMenu.css" />
+    </head>
+
+    <body>  
+
+        <div class="dropdownMenu">
+            Hello!?
+        </div>
+        
+    </body>
+
+</html>
