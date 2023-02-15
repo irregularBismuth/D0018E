45ee7c7da33de31966fb2a0d30369946c1de0907
wiiggee1 <viktor@smedberg.eu>
2023-02-15 13:07:18 +0000

    auto commit

--- productPage.php
+++ productPage.php
@@ -2,6 +2,7 @@
 <?php
 session_start();
 require_once("userProfile.php");
+$userProfile = new UserProfile($sqlHandler);
 require_once("sqlHandler.php");
 $animals = $sqlHandler->get_product_data();
 $row_count = count($animals['image']);
