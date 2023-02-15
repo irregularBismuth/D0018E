45231a669aaabbe8c50871d4efd76c8e91f31aa1
IrregularBismuth <elliot.s@live.se>
2023-02-15 12:40:58 +0000

    auto commit

--- userProfile.php
+++ userProfile.php
@@ -22,7 +22,7 @@ session_start();
             $this->user_id = $_SESSION['id']; 
             }  
         //  $this->userData = array("name"=>"","username"=>"","balance"=>"","profileImage"=>"");//$this->username,"balance"=>$this->balance,"profileImage"=>$this->profileImage);
-           // $this->fetchUserData();
+            $this->fetchUserData();
 
             // FIX: set the global session variables in the function below!
         }
