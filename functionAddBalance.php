2ff7e39930dc29a758ecdc5eaffa04b71e341c53
wiiggee1 <viktor@smedberg.eu>
2023-02-19 21:41:43 +0000

    auto commit

--- functionAddBalance.php
+++ functionAddBalance.php
@@ -3,7 +3,8 @@ session_start();
 require_once("userProfile.php");
 $balance_to_add = $_POST['balance'];
 $userProfile->addBalance($balance_to_add);
-if(isset($_POST['balance'])){
+
+if(isset($_POST['submit_balance'])){
     header("Location: userMenu.php"); 
     exit(0); 
 } 
