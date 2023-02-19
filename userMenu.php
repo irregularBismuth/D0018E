da125ef8f434d7dbca50d1cf1fe9eef0dc7d3c92
wiiggee1 <viktor@smedberg.eu>
2023-02-19 11:44:29 +0000

    auto commit

--- userMenu.php
+++ userMenu.php
@@ -10,18 +10,20 @@ require_once("userProfile.php");
         <link rel="stylesheet" type="text/css" href="style/userMenu.css" />
     </head>
 
-    <body>  
+    <body> 
+            <div>
+
+                <?php echo $userProfile->getSessionData()['name']."<br>";
+                    echo $userProfile->getSessionData()['balance']."<br>"; 
+                ?>
+            </div>
 
         <?php 
-            echo $userProfile->getSessionData()['name']."<br>";
-            echo "Balance: ".$userProfile->getSessionData()['balance']."<br>";
-            
-
+                                           
             echo "<form action='uploadProfileImage.php' method='POST' enctype='multipart/form-data'>
                         <input type='file' name='file'>
                         <button type='submit' name='submit' > upload image </button>
-                    </form>";
-               
+                    </form>";              
         ?>
         
     </body>
