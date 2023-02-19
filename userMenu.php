14dec0e6ae0a1496a9f5082d87b7d1f7dd794244
wiiggee1 <viktor@smedberg.eu>
2023-02-19 18:13:14 +0000

    auto commit

--- userMenu.php
+++ userMenu.php
@@ -15,7 +15,7 @@ require_once("userProfile.php");
         <ul>
             Items here....?
             <li>
-            <?php echo '<img class="profileStyle" src="'.$userProfile->getSessionData()['profileImage'].'" />'; ?>
+            <?php echo '<img class="profileStyle" src="'.$userProfile->getSessionData()['profileImage'].' height=50 width=50" />'; ?>
                 <ul>
                     <li class="submenu_item">
                         <span class="menu_icons">Item1</span>
