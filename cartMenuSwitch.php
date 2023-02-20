996f0bbb17fda717fe55e2989bf910f8b62a31d2
wiiggee1 <viktor@smedberg.eu>
2023-02-20 17:44:09 +0000

    auto commit

--- cartMenuSwitch.php
+++ cartMenuSwitch.php
@@ -18,7 +18,7 @@ session_start();
                 echo '
                     <div class="productCart">
                         <a href="productPage.php">
-                        <img src="../images/animalIcon.png" alt ="Image" width=80, height=78>
+                        <img src="../images/animalIcon.png" alt ="Image" width=80, height=74>
                         </a>
                         <br>
                         Products
