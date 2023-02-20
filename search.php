3f91142987fe15dff9e0833a4da7d5b75a781605
IrregularBismuth <elliot.s@live.se>
2023-02-20 10:58:04 +0000

    auto commit

--- search.php
+++ search.php
@@ -5,7 +5,7 @@ require_once "sqlHandler.php";
 
 $input=$_GET['q'];
             $arr=array($input);
-            $query="select * from animals where animal_name like %:x";
+            $query="select * from animals where animal_name like :x%";
             $sqlHandler->half_genericQuery($query,1,$arr);
             $res=$sqlHandler->s->fetchAll();
             if($sqlHandler->s->rowCount() > 0)
