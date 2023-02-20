d5b1c943b6c11f06834d4b4f9a4ebca096217b0b
wiiggee1 <viktor@smedberg.eu>
2023-02-20 10:22:02 +0000

    auto commit

--- userMenu.php
+++ userMenu.php
@@ -35,7 +35,6 @@ require_once("userProfile.php"); //
                             </p>
                         </form>
                     </li>
-                   <br>
                    <br> 
                     <li class="submenu_item">
                         <form action='uploadProfileImage.php' method='POST' enctype='multipart/form-data'>
