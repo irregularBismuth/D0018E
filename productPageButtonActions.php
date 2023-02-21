be056825f5403c91916e78dead4c7a1308497436
wiiggee1 <viktor@smedberg.eu>
2023-02-21 11:10:52 +0000

    auto commit

---
+++ productPageButtonActions.php
@@ -0,0 +1,14 @@
+<?php 
+session_start();
+
+$buttonAdd = $_POST['addButton'];
+$buttonInfo = $_POST['infoButton'];
+
+function addProductToCartView(){
+    require_once('userProfile.php');
+    if(isset($_POST['addButton'])){
+        //Call userProfile class
+    }
+}
+
+?>
