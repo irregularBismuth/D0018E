350be395bd2e64689170f5eb11cb62052b7f2d94
wiiggee1 <viktor@smedberg.eu>
2023-02-23 18:04:31 +0000

    auto commit

---
+++ transactionalMenu.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+require_once("transactionalHandler.php");
+
+?>
+
+<html>
+
+    <head>
+        <title> Transactional Cart Menu </title>
+        
+    </head>
+    
+    <body>
+        <ul>
+            <li>
+                    <img class="" src="" />
+                <ul>
+                    <li>
+                        <?php echo "method call here for showing cart with products"; ?>
+                    </li>
+                
+                </ul>
+            </li>
+        </ul>
+    
+    </body>
+</html>
