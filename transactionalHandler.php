147305d28308740076862b7e61af6562ce726888
wiiggee1 <viktor@smedberg.eu>
2023-03-05 13:10:52 +0000

    auto commit

--- transactionalHandler.php
+++ transactionalHandler.php
@@ -55,7 +55,7 @@ class TransactionalHandler{
                 array_push($_SESSION['product_cart'], $product_id);
                 $this->product_cart = $_SESSION['product_cart'];
                 //$this->products_added = array('animal_id' => $product_id);
-                header('location: '.$_SERVER['REQUEST_URI']); 
+                //header('location: '.$_SERVER['REQUEST_URI']); 
             }
         }
     }
