a714c88f624ac507a88906c82f489d555f49ab5f
wiiggee1 <viktor@smedberg.eu>
2023-03-05 11:49:32 +0000

    auto commit

--- transactionalHandler.php
+++ transactionalHandler.php
@@ -73,7 +73,7 @@ class TransactionalHandler{
              
             foreach($product_ids as $product_id){
                 $product_data = $this->getProductData($product_id);
-                echo $product_data;
+                echo $product_id;
                 echo '<li class="submenu_item">';
                 echo '<p>'.$product_data["animal_image"].'</p>';
                 echo '<p>'.$product_data["animal_name"].'</p>';
