5b2ffe9260bc0dbe910e99407f32d35251e491fa
IrregularBismuth <elliot.s@live.se>
2023-03-07 15:01:44 +0000

    auto commit

--- comments.php
+++ comments.php
@@ -7,13 +7,14 @@ if(!(isset($_SESSION['username'])))
     header("Location: login.php?bad=2");
     exit(0);
 }
+
 $text=$_POST['comment'];
 $uname=$_POST['name'];
 $anmid=$_POST['animal_id'];
 $sql="insert into comments(animal_id,parent_comment_id,comment,comment_username) values(:x,0,:y,:z)";
 $arr=array($anmid,$text,$name);
 $sqlHandler->half_genericQuery($sql,3,$arr);
-$res=$sqlHandler->s->fetchAll();
+//$res=$sqlHandler->s->fetchAll();
 
 header("Location: page.php?a=".$anmid.");
 exit(0);
