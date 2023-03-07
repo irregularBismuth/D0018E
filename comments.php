d528a3208b2aa7a81844e6fe7d8843fc52f785a7
IrregularBismuth <elliot.s@live.se>
2023-03-07 15:14:24 +0000

    auto commit

--- comments.php
+++ comments.php
@@ -1,12 +1,11 @@
 <?php
 session_start();
 require_once "sqlHandler.php";
-
 $text=$_POST['comment'];
 $uname=$_POST['name'];
 $anmid=$_POST['animal_id'];
 $query="insert into comments(animal_id,parent_comment_id,comment,comment_username) values(:x,0,:y,:z)";
-$arr=array($anmid,$text,$name);
+$arr=array($anmid,$text,$uname);
 $sqlHandler->half_genericQuery($quer,3,$arr);
 //$res=$sqlHandler->s->fetchAll();
 header("Location: page.php?a=".$anmid.");
