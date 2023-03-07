515619b6daf213acf9886c4b8cb028ec79b55952
IrregularBismuth <elliot.s@live.se>
2023-03-07 15:08:55 +0000

    auto commit

--- comments.php
+++ comments.php
@@ -2,15 +2,13 @@
 session_start();
 require_once "sqlHandler.php";
 
-if(!(isset($_SESSION['username'])))
-{
-    header("Location: login.php?bad=2");
-    exit(0);
-}
-
 $text=$_POST['comment'];
 $uname=$_POST['name'];
 $anmid=$_POST['animal_id'];
+
+if(isset($_POST['subknapp']))
+{
+
 $quer="insert into comments(animal_id,parent_comment_id,comment,comment_username) values(:x,0,:y,:z)";
 $arr=array($anmid,$text,$name);
 echo $text;
@@ -18,7 +16,7 @@ echo "</br>".$name;
 echo "</br>".$anmid;
 $sqlHandler->half_genericQuery($quer,3,$arr);
 //$res=$sqlHandler->s->fetchAll();
-
+}
 header("Location: page.php?a=".$anmid.");
 exit(0);
 
--- page.php
+++ page.php
@@ -78,8 +78,7 @@ function search() {
          ?>
         <div class="comments">
             <form action="comments.php" method="POST"> 
-                <textarea class="comment" name="comment" id="comment" placeholder="Comment here"></textarea> 
-                
+                <textarea class="comment" name="comment" id="comment" placeholder="Comment here"></textarea>  
                 <input type="hidden" name="name" value="<?php echo $_SESSION['username']; ?>" />
                 <input type="hidden" name="animal_id" value="<?php echo $who; ?>" />
                 <input type="submit" name="subknapp" value="Comment"/>
