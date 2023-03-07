46845da96ff8b58a9812fa2374b3ad4486f5ea22
IrregularBismuth <elliot.s@live.se>
2023-03-07 15:28:46 +0000

    auto commit

--- comments.php
+++ comments.php
@@ -5,8 +5,12 @@ require_once "function.php";
 
 $name=$_POST['name'];
 $pass=$_POST['comment'];
-echo $pass;
-#$arr=array($name,$pass,$email);
-#$sql="insert into comments (name,password,email) values (:x,:y,:z)";
-#$sqlHandler->half_genericQuery($sql,3,$arr);
+$ani=$_POST['animal_id'];
+$ani=filter($ani);
+$name=filter($name);
+$pass=filter($pass);
+
+$arr=array($ani,$pass,$name);
+$sql="insert into comments (animal_id,comment,comment_username) values (:x,:y,:z)";
+$sqlHandler->half_genericQuery($sql,3,$arr);
 ?>
