9f7a08fb8b15f4e74f24d32b62230008a586ba6c
IrregularBismuth <elliot.s@live.se>
2023-03-09 23:50:19 +0000

    auto commit

--- delcom.php
+++ delcom.php
@@ -2,11 +2,11 @@
 session_start();
 require_once "sqlHandler.php";
 $delid=$_POST['comid'];
+$anmid=$_POST['aid'];
 $arr=array($delid);
 $quer="delete from comments where comment_id=:x";
 $sqlHandler->half_genericQuery($quer,1,$arr);
-$headl="Location: page.php?a=".$_POST['aid'];
-header($headl);
+header("Location: page.php?a=".$anmid);
 exit(0);
 
 
