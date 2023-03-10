b772353e2634d03c3eee369e1af6a51d1757f65f
wiiggee1 <viktor@smedberg.eu>
2023-03-10 03:32:49 +0000

    auto commit

--- transactionalHandler.php
+++ transactionalHandler.php
@@ -108,7 +108,7 @@ class TransactionalHandler{
             
 
             $product_ids = $this->getProductCart($session_order_id); // check the [0] index! 
-            echo $product_ids['product_id']; 
+            echo $product_ids['product_id'][0]; 
             //$this->updateCartDisplay($_POST['product_id_cart']); 
            
             $subtotal = 0;
