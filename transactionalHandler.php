643853a6f21bfdf9ce593736a1124677d94fb668
wiiggee1 <viktor@smedberg.eu>
2023-03-10 13:10:56 +0000

    auto commit

--- transactionalHandler.php
+++ transactionalHandler.php
@@ -178,8 +178,8 @@ class TransactionalHandler{
     
     function updateCartDisplay($product_id_to_remove){
         if (isset($_POST['removeButton'])){
-            $item_to_remove = array_search($product_id_to_remove, $_SESSION['product_cart']);
-            unset($_SESSION['product_cart'][$item_to_remove]);
+            $item_to_remove = array_search($product_id_to_remove, $_SESSION['product_cart']['product_id']);
+            unset($_SESSION['product_cart']['product_id'][$item_to_remove]);
             //$this->generateCartDisplay();
             header('location: '.$_SERVER['REQUEST_URI']); 
         }
