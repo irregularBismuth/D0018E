a5d2a179791b918b9eaa0b0d2c4dd55018aa98c8
IrregularBismuth <elliot.s@live.se>
2023-03-10 00:10:45 +0000

    auto commit

--- delete.php
+++ delete.php
@@ -3,12 +3,10 @@ session_start();
 require_once "sqlHandler.php";
 $quer="delete from animals where id=:x";
 $anmid=$_POST['animal_id'];
-echo $anmid;
+$arr=array($anmid);
+$sqlHandler->half_genericQuery($quer,1,$arr);
 
-
-
-
-
-
+header("Location: productPage.php");
+exit(0);
 
 ?>
