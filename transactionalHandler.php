eb90a97c1c0d06aaeb1a62fea0e231fbe4f23a42
wiiggee1 <viktor@smedberg.eu>
2023-03-10 00:41:19 +0000

    auto commit

--- transactionalHandler.php
+++ transactionalHandler.php
@@ -91,9 +91,11 @@ class TransactionalHandler{
                 echo '|';
                 echo '<p> total: '.$product_data["animal_price"]*$product_quantity.'¥</p>';
                 echo '<br>';
+                echo '<li class="submenu_item">';
                 echo '<form style="display: block; background-color: inherit;" method="POST">';
                 echo '<input type="hidden" name="product_id_cart" value='.$product_data["animal_id"].' />';
                 echo '<input type="number" name=product_quantity value="1" max='.$product_quantity.'min="1"/>';
+                echo '</li>';
                 echo '<br>';
                 echo '<li class="submenu_item">';
                 echo '<button type="submit" name="removeButton" value="remove">';
