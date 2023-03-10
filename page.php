d44ce5aa4052bc9dd04347f43b11fad4fb386d76
IrregularBismuth <elliot.s@live.se>
2023-03-10 00:19:38 +0000

    auto commit

--- page.php
+++ page.php
@@ -102,7 +102,7 @@ function search() {
                     foreach($rez as $rez){
                         $sr="";
                         if($_SESSION['admin']==1){
-                        $sr="<form action='delcom.php' mehod='post'><input type='hidden' name='aid' value=".$who."><input type='hidden' name='comid' value=".$rez['comment_id']."><input type='submit'></form>"; } 
+                        $sr="<form action='delcom.php' mehod='get'><input type='hidden' name='aid' value=".$who."><input type='hidden' name='comid' value=".$rez['comment_id']."><input type='submit'></form>"; } 
                         echo "<div class='com'><span style='font-weight:bold'>".$rez['comment_username']."</span></p><p>".$rez['comment_time']."</p><p>".$rez['comment']."</p>".$sr."</div>";
                     }     
                }
