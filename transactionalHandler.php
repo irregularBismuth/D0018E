c5ae21e1b2ca1743373447f1968c5d779e1a1de7
wiiggee1 <viktor@smedberg.eu>
2023-03-10 03:29:23 +0000

    auto commit

--- transactionalHandler.php
+++ transactionalHandler.php
@@ -101,14 +101,14 @@ class TransactionalHandler{
     function generateCartDisplay(){
         
         
-
             $query_order_id = "SELECT order_id FROM transactional WHERE customer_id=:x";
             $userid_param = array($_SESSION['id']);
             $this->sqlConnector->half_genericQuery($query_order_id, 1, $userid_param);         
             $session_order_id = $this->sqlConnector->s->fetch(PDO::FETCH_ASSOC);
             
 
-            $product_ids = $this->getProductCart($session_order_id)['product_id']; // check the [0] index! 
+            $product_ids = $this->getProductCart($session_order_id); // check the [0] index! 
+            echo $product_ids; 
             //$this->updateCartDisplay($_POST['product_id_cart']); 
            
             $subtotal = 0;
