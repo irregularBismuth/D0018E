82b0bf9f2e6db2ae69991e2dc52ce17517479c7a
wiiggee1 <viktor@smedberg.eu>
2023-03-10 14:36:32 +0000

    auto commit

--- transactionalHandler.php
+++ transactionalHandler.php
@@ -63,7 +63,7 @@ class TransactionalHandler{
                 $param_insert = array($session_transactional['order_id'], $items['animal_id'], 1);
                 $this->sqlConnector->half_genericQuery($insert_query, 3, $param_insert);
 
-                $_SESSION['product_cart'][] = array('product_id'=>$items['animal_id'], 'order_id'=>$session_transactional['order_id'] , 'order_quantity'=>1);
+                $_SESSION['product_cart'] = array('product_id'=>$items['animal_id'], 'order_id'=>$session_transactional['order_id'] , 'order_quantity'=>1);
             }
             
             if(isset($_SESSION['product_cart'])){
