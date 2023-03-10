af3c6d12fecdb0a00b4296ef78c94f24cd2c1d9c
IrregularBismuth <elliot.s@live.se>
2023-03-10 03:44:09 +0000

    auto commit

--- testCart.php
+++ testCart.php
@@ -8,7 +8,7 @@ $sqlHandler->half_genericQuery($quer,0,$ar);
 $res=$sqlHandler->fetchAll();
 foreach($res as $res)
 {
-    echo "<form action='addCart.php' method='post'><label>\"$res['animal_name']\"</label><input type='number' name='anum'><input type='submit'></form></br></br>";
+    echo "<form action='addCart.php' method='post'><label>.$res['animal_name']."</label><input type='number' name='anum'><input type='submit'></form></br></br>";
 }
 
 ?>
