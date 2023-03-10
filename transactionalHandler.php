c10500d8b6d076b7835153707e957860dc4fdf95
IrregularBismuth <elliot.s@live.se>
2023-03-10 08:29:48 +0000

    auto commit

--- transactionalHandler.php
+++ transactionalHandler.php
@@ -212,7 +212,7 @@ class TransactionalHandler{
             $sql_metadata_transactional = "INSERT INTO transactional (shoppingCart_bool, customer_id, transactional_amount, comment) VALUES (:x, :y, :z, :w)"; 
             $t_param = array(0, $userid, 0, "no status");
             $output = $this->sqlConnector->half_genericQuery($sql_metadata_transactional, 4, $t_param);
-            $output->s->execute();
+    
     }
     
     public function execTransaction($userid, $product_ids, $total_amount){
--- transactionalHandler2.php
+++ transactionalHandler2.php
@@ -2,104 +2,27 @@
 session_start();
 require_once("sqlHandler.php");
 require_once("userProfile.php");
-$sql = $sqlHandler;
-$transactionalHandler = new TransactionalHandler($sql);
-?>
 
-<?php 
-class TransactionalHandler{
-    private $product_cart;
-    private $sqlConnector;
-    private $session_order_id;
-    private $customer_id;
-    private $product_id; 
 
-    function __construct($sqlConnectorReference)
+function addToCart()
+{
+    $quer="select * from cart where customer_id=:x";
+    $arr=array($_SESSION['id']);
+    $sqlHandler->half_genericQuery($quer,1,$arr);
+    $res=$sqlHandler->s->fetchAll();
+    if(empty($res))
     {
-        session_start();
-        $this->product_cart = array();
-        $this->sqlConnector = $sqlConnectorReference;
-        $this->session_order_id = "";
-        $this->customer_id = "";
-        $this->product_id = array();
-        //product_id and button_id... 
+        $c="insert into cart(customer_id) values(:x)";
+        $sqlHandler->half_genericQuery($c,1,$arr);
+        $cid=$sqlHandler->s->getLastInsertedID();
+    }else {
+        $cid=res[0]['id'];
         
     }
-
-    function checkCustomerId(){
-        require_once("userProfile.php");
-        
-        if($userProfile->checkIfUserIdSet()){ 
-            $_SESSION['customer_id'] = $_SESSION['id'];
-            $this->customer_id = $_SESSION['id'];            
-            
-        }
-        else {          
-                $this->customer_id = session_create_id();
-                $_SESSION['customer_id'] = $this->customer_id;
-            }
-    }
-
-     function addToCart() {
-    $customer_id=$_SESSION['id'];
-    $price_per_item=$_POST['price'];
-    $quantity=1;
-    $product_id=$_POST['product_id'];
-    // Check if the customer already has a cart
-    $cart_query = "SELECT * FROM cart WHERE customer_id = :x";
-    $cart_data = $this->sqlConnector->half_genericQuery($cart_query, 1, array($customer_id));
-
-    if (empty($cart_data)) {
-        // If the customer does not have a cart, create one
-        $cart_insert_query = "INSERT INTO cart (customer_id) VALUES (:x)";
-        $this->sqlConnector->half_genericQuery($cart_insert_query, 1, array($customer_id));
-        $cart_id = $this->sqlConnector->getLastInsertedID();
-    } else {
-        $cart_id = $cart_data[0]['id'];
-    }
-
-    // Check if the product already exists in the cart
-    $cart_item_query = "SELECT * FROM cart_item WHERE cart_id = :x AND product_id = :y";
-    $cart_item_data = $this->sqlConnector->half_genericQuery($cart_item_query, 2, array($cart_id, $product_id));
-
-    if (empty($cart_item_data)) {
-        // If the product does not exist in the cart, add it
-        $cart_item_insert_query = "INSERT INTO cart_item (cart_id, product_id, quantity, price) VALUES (:x, :y, :z, :w)";
-        $this->sqlConnector->half_genericQuery($cart_item_insert_query, 4, array($cart_id, $product_id, $quantity, $price_per_item));
-    } else {
-        // If the product already exists in the cart, update its quantity
-        $new_quantity = $cart_item_data[0]['quantity'] + $quantity;
-        $cart_item_update_query = "UPDATE cart_item SET quantity = :x WHERE cart_id = :y AND product_id = :z";
-        $this->sqlConnector->half_genericQuery($cart_item_update_query, 3, array($new_quantity, $cart_id, $product_id));
-    }
+    $ciq="select * from cart_item where cart_id=:x and product_id=:y";
+    $sqlHandler->half_genericQuery($ciq,2,array())
 } 
-       /* $quer="select * from cart where customer_id = :x";
-        $arr=(array($_SESSION['id']));
-        $this->sqlConnector->half_genericQuery($quer,1,$arr);
-        $res=$this->sqlConnector->s->fetchAll();
-      //  foreach($res as $res) { }
-        if($res===false)
-        {
-            $query="insert into carts(customer_id) values(:x)";
-            $arr2=(array($_SESSION['id']));
-            $this->sqlConnector->s->half_genericQuery($query,1,$arr2);
-            $res=$this->sqlConnector->s->lastInsertId();
-        
-        }
-        else {
-            $res=res['cart_id'];
-        }
-
-        $quee="insert into cart_item(cart_id,product_id,quantity,price) values(:x,:y,:z,:w)";
-        $arre=array($res,$_POST['product_id'],1,$_POST['price']);
-        $rez=$this->sqlConnector->s->half_genericQuery($quee,4,$arre);
-        */ 
- 
     
     
-    function checkout()
-    {
 
-    }
-}
 ?>
