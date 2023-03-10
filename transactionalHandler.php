38264354b3e08d076926a4a7162c7ba62224ce8a
wiiggee1 <viktor@smedberg.eu>
2023-03-10 13:07:50 +0000

    auto commit

--- transactionalHandler.php
+++ transactionalHandler.php
@@ -121,7 +121,7 @@ class TransactionalHandler{
                 $subtotal += $product_data['animal_price']; 
                 $product_quantity = $product_data['animal_quantity'];
                 echo '<pre>';
-                //var_dump($_SESSION['product_cart']);
+                echo $product_data;
                 echo '<li class="submenu_item">';
                 echo '<img class="submenu_item" src='.$product_data["animal_image"].'>';
                 echo '<p> product: '.$product_data["animal_name"].'</p>';
