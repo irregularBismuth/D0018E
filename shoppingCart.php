b785cbde077feb7347bed0488976df26aff05f2d
IrregularBismuth <elliot.s@live.se>
2023-03-12 20:09:48 +0000

    auto commit

--- shoppingCart.php
+++ shoppingCart.php
@@ -14,10 +14,13 @@ require_once "sqlHandler.php";
     $quer="select * from animals";
     $sqlHandler->half_genericQuery($quer,0,0);  
     $res=$sqlHandler->s->fetchAll();
+    echo "<form action='scHandler.php' method='post'>";
     foreach($res as $res){
+        
         echo $res['animal_id'];
     }
-   echo $str; 
+    echo "<input type='submit' />";
+   echo "</form>"; 
 ?>
 </body>
 
