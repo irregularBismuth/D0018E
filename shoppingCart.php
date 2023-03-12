c38b3871f95cc39abb576c1bfd1272128575dfc8
IrregularBismuth <elliot.s@live.se>
2023-03-12 20:38:25 +0000

    auto commit

--- shoppingCart.php
+++ shoppingCart.php
@@ -7,6 +7,7 @@ require_once "sqlHandler.php";
 
 <html>
  <head>
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
  </head>
     
 <body>
@@ -18,7 +19,7 @@ require_once "sqlHandler.php";
         echo "<div class='animal'><img src=".$res['animal_image']."><p>".$res['animal_name']."</p><p>".$res['animal_price']."</p>";
         echo "<form action='scHandler.php' method='post'>";
         echo "<input type='hidden' name='anmid' value=".$res['animal_id']."><input type='submit' value='Add'>"; 
-        echo "</form></div>"; 
+        echo "</form></div>";  
    
      }
 ?>
