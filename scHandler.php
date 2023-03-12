2e00247d60cda930a3b4b9d37bb986d349189a49
IrregularBismuth <elliot.s@live.se>
2023-03-12 21:22:13 +0000

    auto commit

--- scHandler.php
+++ scHandler.php
@@ -13,7 +13,15 @@ if($sqlHandler->s->rowCount() > 0)
 else {
     $query="insert into cart(customer_id) values(:x)";
     $sqlHandler->half_genericQuery($query,1,array($_POST['uid']));
-    
+    $querry="select * from cart where customer_id=:x";
+    $sqlHandler->half_genericQuery($querry,1,array($_POST['uid']));
+    $res=$sqlHandler->s->fetchAll();
+    foreach ($res as $res){
+        $cartid=$res['id'];
+    }
+    $quer="insert into cart_item(cart_id,product_id,price) values(:x,:y,:z)";
+    $sqlHandler->half_genericQuery($quer,3,array($cartid,$id,$_POST['price']));
+    $res=$sqlHandler->s->fetchAll();
     
 }
 
--- shoppingCart.php
+++ shoppingCart.php
@@ -18,7 +18,7 @@ require_once "sqlHandler.php";
     foreach($res as $res){
         echo "<div class='animal'><img src=".$res['animal_image']."><p>".$res['animal_name']."</p><p>".$res['animal_price']."</p>";
         echo "<form action='scHandler.php' method='post'>";
-        echo "<input type='hidden' name='uid' value=".$_SESSION['id']." ><input type='hidden' name='anmid' value=".$res['animal_id']."><input type='submit' value='Add'>"; 
+        echo "<input type='hidden' name='uid' value=".$_SESSION['id']." ><input type='hidden' name='price' value=".$res['animal_price']."><input type='hidden' name='anmid' value=".$res['animal_id']."><input type='submit' value='Add'>"; 
         echo "</form></div>";  
    
      }
