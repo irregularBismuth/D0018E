7b3b044f922febc3e7fd20fa53068eec53a2e354
IrregularBismuth <elliot.s@live.se>
2023-03-12 20:58:26 +0000

    auto commit

--- scHandler.php
+++ scHandler.php
@@ -1,5 +1,17 @@
 <?php
 session_start();
-echo $_POST['anmid'];
+require_once "sqlHandler.php";
 
+$id=$_POST['anmid'];
+$quer="select * from cart where customer_id=:x";
+$sqlHandler->half_genericQuery($quer,1,array($_POST['uid']));
+$res=$sqlHandler->s->fetchAll();
+if($sqlHandler->s->rowCount() > 0)
+{
+ 
+}
+
+
+//header("Location: shoppingCart.php");
+//exit(0);
 ?>
