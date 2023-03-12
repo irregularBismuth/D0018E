eb3c6991cf112c41295c2273eb66ab2f19e3d408
IrregularBismuth <elliot.s@live.se>
2023-03-12 20:06:12 +0000

    auto commit

--- shoppingCart.php
+++ shoppingCart.php
@@ -14,11 +14,9 @@ require_once "sqlHandler.php";
     $quer="select * from animals";
     $sqlHandler->half_genereicQuery($quer,0,0);  
     $res=$sqlHandler->s->fetchAll();
-    $str="<form action='scHandler.php' method='post'>";
     foreach($res as $res){
-        $str=$str."<input type='number' name='anmquanity' /><input type='hidden' value=".$res['animal_id']."><input type='button' value='addToCart' name='animalButton' /></br>"; 
+        echo $res['animal_id'];
     }
-    $str=$str."</form>";
    echo $str; 
 ?>
 </body>
