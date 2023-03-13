8ac34e32e6d2f2429510eee87725e9f3e41896ad
IrregularBismuth <elliot.s@live.se>
2023-03-13 18:40:08 +0000

    auto commit

--- removeCart.php
+++ removeCart.php
@@ -4,10 +4,16 @@ require_once "sqlHandler.php";
 $aid=$_POST['aid'];
 $cid=$_POST['cid'];
 
+if($aid > 1){
+$aid-=1;
 $query="update cart_item set quantity=:x where id=:y";
-if($aid >= 1){
-$aid-=1;}
 $sqlHandler->half_genericQuery($query,2,array($aid,$cid));
+}
+else{
+   $query="delete cart_item where id=:x";
+   $sqlHandler->half_genericQuery($query,1,array($cid));
+}
+
 header("Location: shoppingCart.php");
 exit(0);
 ?>
