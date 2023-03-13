164b531a3eb5306a36592426c43a1a28fd60af03
IrregularBismuth <elliot.s@live.se>
2023-03-13 18:36:47 +0000

    auto commit

--- removeCart.php
+++ removeCart.php
@@ -5,12 +5,9 @@ $aid=$_POST['aid'];
 $cid=$_POST['cid'];
 
 $query="update cart_item set quantity=:x where id=:y";
-
 if($aid >= 1){
 $aid-=1;}
-
 $sqlHandler->half_genericQuery($query,2,array($aid,$cid));
-
 header("Location: shoppingCart.php");
 exit(0);
 ?>
