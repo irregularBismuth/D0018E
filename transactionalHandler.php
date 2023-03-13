bfb01a61cc30f08986e007d46b602ada93bce157
wiiggee1 <viktor@smedberg.eu>
2023-03-13 16:08:52 +0000

    auto commit

--- transactionalHandler.php
+++ transactionalHandler.php
@@ -129,8 +129,8 @@ class TransactionalHandler{
                     $initid=$output['id'];
                 }
 
-                $query = "SELECT * FROM animals join cart_item on animals.animals_id = cart_item.product_id";
-                //$query = "SELECT * FROM animals, cart_item where cart_id=:x";
+                //$query = "SELECT * FROM animals join cart_item on animals.animals_id = cart_item.product_id";
+                $query = "SELECT * FROM animals, cart_item where cart_id=:x";
                 $sqlHandler->half_genericQuery($query, 1, array($initid));
                 $output = $sqlHandler->s->fetchAll();
                 $tot=0;
