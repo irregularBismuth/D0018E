bf9acf4fe0feb142eb070e760fc02942ff0ca2b9
wiiggee1 <viktor@smedberg.eu>
2023-03-14 08:18:21 +0000

    auto commit

--- transactionalHandler.php
+++ transactionalHandler.php
@@ -191,6 +191,8 @@ class TransactionalHandler{
                 $initid = $output['id'];
             }
 
+            // DELETE AND CHECK IF QUANTITY IS BIGGER THAN 1 THEN REMOVE ONE QUANTITY ELSE REMOVE ITEM FROM CART
+
             //$query = "SELECT * FROM cart_item JOIN animals ON cart_item.product_id = animals.animal_id WHERE cart_id=:x";
             $query = "delete from cart_item where product_id=:x";
             $this->sqlConnector->half_genericQuery($query, 1, array($product_id_to_remove));
