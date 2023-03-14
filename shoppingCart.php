b6936e75183d8bab3293bd1adecd95cfc1a738d1
IrregularBismuth <elliot.s@live.se>
2023-03-14 15:00:35 +0000

    auto commit

--- shoppingCart.php
+++ shoppingCart.php
@@ -18,62 +18,6 @@ $succ=$_GET['succ'];
     
 <body>
 
-<div class="headern">
-<header>
- 
-    <a href="site.php"><img src="../images/logo.png" width="400"></a>
-    <?php generateCartButton(); ?>
-    <?php include("userMenu.php"); ?>
-    
-
-    
-<script>
-function search() {
-    
-    var str=document.getElementById("fname").value;
-    if (str == "") {
-        document.getElementById("livesearch").innerHTML = "";
-        document.getElementById("livesearch").style.display="none";
-        return;
-    }
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest();
-    }
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            document.getElementById("livesearch").innerHTML = xmlhttp.responseText;
-            document.getElementById("livesearch").style.display ="block";
-        }
-    }
-    xmlhttp.open("GET", "search.php?q=" + str, true);
-    xmlhttp.send();
-}
-
-</script>
-<form>
-    <input type="text" size="30" id="fname" onkeyup="search()">
-</form>
-<!--
-<form role="search" id="form">
-  <input type="search" id="query" name="q"
-   placeholder="Search..."
-   aria-label="Search through site content">
--->
-  <button>
-    <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
-  </button>
-    <?php
-    if(!(isset($_SESSION['username']))){
-    echo "<a href='login.php'> Login </a>";
-    }
-    else {
-    echo "<a href='logoutCheck.php'> Logout </a>";
-    }
-    ?>
-</header>
-</div>
-           
-<div id="livesearch" class="livesearch"></div>
 
 <?php
     if($succ==1){
