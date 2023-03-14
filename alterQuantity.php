107aea7de8305b683299213d0476d59ac86c8d77
IrregularBismuth <elliot.s@live.se>
2023-03-14 23:04:45 +0000

    auto commit

--- alterQuantity.php
+++ alterQuantity.php
@@ -7,6 +7,24 @@ $n=$_POST['num'];
 $milker="update ode_item set quantity=:x where id=:y";
 $sqlHandler->half_genericQuery($milker,2,array($n,$id));
 
+$milch="select * from ode_item where quantity=:x and id=:y";
+$sqlHandler->half_genericQuery($milch,2,array($n,$id));
+$zz=$sqlHandler->s->fetchAll();
+$odeid=$zz['odeid'];
+$milch="select * from ode_item where odeid=:x";
+$sqlHandler->half_genericQuery($milch,1,array($odeid));
+$ww=$sqlHandler->s->fetchAll();
+$tot=0;
+foreach($ww as $ww){
+ $tot+=$ww['price']*$w['quantity'];
+}
+$bate="update ode set price=:x where id=:y";
+$sqlHandler->half_genericQuery($bate,2,array($tot,$odeid));
+
+
+
+
+
 header("Location: showOrders.php");
 exit(0);
 
