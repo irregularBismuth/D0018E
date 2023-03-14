54504843eb4137641302cc731c526aa82db5c3d0
IrregularBismuth <elliot.s@live.se>
2023-03-14 10:03:00 +0000

    auto commit

--- shoppingCart.php
+++ shoppingCart.php
@@ -52,10 +52,16 @@ $bad=$_GET['bad'];
     $sqlHandler->half_genericQuery($quer,0,0);  
     $res=$sqlHandler->s->fetchAll();
     foreach($res as $res){
+    
         echo "<div class='animal'><img src=".$res['animal_image']."><p>".$res['animal_name']."</p><p>".$res['animal_price']."</p>";
+        if($res['animal_quantity'] > 0){
         echo "<form action='scHandler.php' method='post'>";
         echo "<input type='hidden' name='uid' value=".$_SESSION['id']." ><input type='hidden' name='price' value=".$res['animal_price']."><input type='hidden' name='anmid' value=".$res['animal_id']."><input type='submit' value='Add'>"; 
         echo "</form></div>";  
+        }
+        else{
+            echo "Item currently out of stock retry tomorrow";
+        }
    
      }
 ?>
