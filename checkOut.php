aefafc257287a30578f62b1e57a96a02d8c5e776
IrregularBismuth <elliot.s@live.se>
2023-03-14 06:39:58 +0000

    auto commit

--- checkOut.php
+++ checkOut.php
@@ -7,19 +7,38 @@ function check()
 require_once "sqlHandler.php";
     try{
        
-  /*      $query="select * from cart where customer_id=:x";
+        $query="select * from cart where customer_id=:x";
         $sqlHandler->half_genericQuery($query,1,array($id));
-        $res=$sqlHandler->s->fetchAll();
+        $res=$sqlHandler->s->fetchAll(); 
         $yy=0;
-        foreach($res as $res){
-            $yy=$res['id'];
+        if($sqlHandler->s->rowCount() > 0){
+            foreach($res as $res){
+                $yy=$res['id'];
+            }
         }
-   */
+        else{ 
+            return 2;
+        }
+        
          $dbcc=$sqlHandler->get_db_connector();
         $dbcc->beginTransaction();
+
+        $quer="select * from animals,cart_item where cart_item=:x";
+        $sqlHandler->half_genericQuery($query,1,array($yy));
+        $rez=$sqlHandler->s->fetchAll();
+        $tot=0;
+        foreach($rez as $rez){
+            if($rez['product_id']==$rez['animal_id']){
+                $tot+=$rez['quantity']*$rez['price'];
+                if($rez['quantity'] > $rez['animal_quantity']){
+                    return 3;
+                }
+            }
+            
+        }
         
-        $query="insert into cart(customer_id) values(7)";
-        $sqlHandler->half_genericQuery($query,0,0);  
+         
+       // $sqlHandler->half_genericQuery($query,0,0);  
  /*    $query="select * from animals,cart_item where cart_id=:x";
         $sqlHandler->half_genericQuery($query,1,array($yy));
         $rez=$sqlHandler->s->fetchAll();
@@ -38,12 +57,8 @@ require_once "sqlHandler.php";
             exit(0);
         }
         */
-        if(1)
-        {
-            return false;
-        }
         $dbcc->commit();
-      return true;  
+      return 0;  
    //     header("Location: shoppingCart.php");
     //     exit(0);
         //return true;
@@ -54,13 +69,20 @@ require_once "sqlHandler.php";
        // echo ($e->getMessage());    
     }
 }
-if(check()){
+$val=check();
+if($val==0){
 header("Location: shoppingCart.php");
 exit(0);
 }
-else{
+if($val==1){
 header("Location: shoppingCart.php?bad=1");
 exit(0);
 }
+
+if($val==3){
+header("Location: shoppingCart.php?bad=2");
+exit(0);
+}
+
 
 ?>
