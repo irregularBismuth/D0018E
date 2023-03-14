37a30e102187c140f577bf696168d8e199d770ac
IrregularBismuth <elliot.s@live.se>
2023-03-14 12:25:09 +0000

    auto commit

--- review.php
+++ review.php
@@ -1,4 +1,40 @@
 <?php
 session_start();
+require_once "sqlHandler.php";
+$id=$_SESSION['id'];
+$milkerz="select * from ode where customer_id=:x";
+$sqlHandler->half_genericQuery($milkerz,1,array($id));
+$rez=$sqlHandler->s->fetchAll();
 
 ?>
+
+<html>
+<head>
+</head>
+
+<body>
+
+<?php
+
+
+foreach($rez as $rez){
+
+    $odeid=$rez['id'];
+    $milkert="select * from ode_item where odeid=:x";
+    $sqlHandler->half_genericQuery($milkert,1,array($odeid));
+    $ree=$sqlHandler->s->fetchAll();    
+        
+    echo "order number: ".$rez['id']." Ordertime".$rez['dateTime'];
+    foreach($p as $p)
+    {
+        echo "x".$p['quantity']." price: ".$p['price'];    
+    }
+    echo "</br>".$rez['total'];
+    
+}
+
+?>
+
+</body>
+
+</html>
---
+++ showOrders.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require_once "sqlHandler.php";
+
+
+
+?>
+
+<html>
+<head>
+</head>
+<body>
+</body>
+</html>
