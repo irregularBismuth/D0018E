d38e264045abb6168d0d7bc3b54dcb232a610ebb
IrregularBismuth <elliot.s@live.se>
2023-03-14 22:55:58 +0000

    auto commit

--- alterQuantity.php
+++ alterQuantity.php
@@ -1,3 +1,14 @@
 <?php
 session_start();
+require_once "sqlHandler.php";
+$id=$_POST['hid'];
+$n=$_POST['num'];
+
+$milker="update odeitem set quantity=:x where id=:y";
+$sqlHandler->half_genericQuery($milker,2,array($n,$id));
+
+header("Location: showOrders.php");
+exit(0);
+
+?>
 
