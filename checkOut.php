39d64b172b71b815546c2455230509b3766abab7
IrregularBismuth <elliot.s@live.se>
2023-03-14 06:56:08 +0000

    auto commit

--- checkOut.php
+++ checkOut.php
@@ -28,10 +28,9 @@ function check($id)
         }
         
          $dbcc=$sqlHandler->get_db_connector();
-        $dbcc->beginTransaction();
-            
-        
-       $quer="select * from animals,cart_item where animal_id=product_id and cart_id=:x";
+        $dbcc->beginTransaction();    
+         
+        $quer="select * from animals,cart_item where animal_id=product_id and cart_id=:x";
         $sqlHandler->half_genericQuery($query,1,array($yy));
         $rez=$sqlHandler->s->fetchAll();
         $tot=0;
