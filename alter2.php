1f6abd9b354cecec305fa91d0ce3209c23252cc0
IrregularBismuth <elliot.s@live.se>
2023-03-15 11:04:35 +0000

    auto commit

--- alter2.php
+++ alter2.php
@@ -3,7 +3,7 @@ session_start();
 require_once "sqlHandler.php";
 $id=$_POST['aid'];
 $hid=$_POST['hid'];
-$dll="update animals set animal_quantity=:x where animal_id=:y";
+$dll="update animals set animal_price=:x where animal_id=:y";
 $sqlHandler->half_genericQuery($dll,2,array($hid,$id));
 
 header("Location: page.php?a=".$id);
