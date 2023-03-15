49961447349f94c96405493cf2218fcbb268eb0b
IrregularBismuth <elliot.s@live.se>
2023-03-15 09:40:32 +0000

    auto commit

---
+++ alterCurrency.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+require_once "sqlHandler.php";
+$id=$_POST['hid'];
+$num=$_POST['num'];
+
+$milker="update users set balance=:x and id=:y";
+$sqlHandler->half_genericQuery($milker,2,array($num,$id));
+header("Location: superProfile.php?succ=1");
+exit(0);
+
+?>
