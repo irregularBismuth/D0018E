6488d44624fa53bd31b4c6debbe1928a4b53f114
IrregularBismuth <elliot.s@live.se>
2023-03-15 10:06:24 +0000

    auto commit

--- alterUser.php
+++ alterUser.php
@@ -7,24 +7,19 @@ $id = $_POST['hid'];
 $dll="select * from users";
 $sqlHandler->half_genericQuery($dll,0,0);
 $w=$sqlHandler->s->fetchAll();
-
+$s=0;
 foreach($w as $w){
 
 if($newUsername==$w['name'])
 {    // already exist
-
+    $s=1;
 }
 }
 
 //om inte finns ändra user name med ny query where id = $id;
 //
 //
-if((isset($_SESSION['username']))){
-
-header("Location: superProfile.php");
-exit(0);
-}
-else {
+if($s==1){
 
 header("Location: superProfile.php?bad=1");
 exit(0);
