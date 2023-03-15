d947697f10ee59c7470147de4f451a899c1532bc
IrregularBismuth <elliot.s@live.se>
2023-03-15 08:58:32 +0000

    auto commit

--- superProfile.php
+++ superProfile.php
@@ -7,9 +7,15 @@ $dll="select * from users";
 $sqlHandler->half_genericQuery($dll,0,0);
 $w=$sqlHandler->s->fetchAll();
     foreach($w as $w){
-     echo "<form type=''>" 
+     echo "<h2>USER: ".$w['name']."</h2>"
+     echo "<form type=''></form>";
      echo "<form type='deleteUser.php' method='post'><input type='hidden' value=".$w['id']."><input type='submit' value='deleteAccount'></form>";
     }
+}
+else {
+    $dll="select * from users where id=:x";
+    $sqlHandler->half_genericQuery($dll,1,array($_SESSION['id']));
+    
 }
 
 
